86d217013bbf8690500a2c02099fc38f062ddb4e
Chris Mazuc <chris@mazuc.net>
2015-04-10 08:05:38 -0700

    test

---
+++ wsgi.py
@@ -0,0 +1,4 @@
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8')])
+    return ['<!DOCTYPE html><html><meta charset="utf-8"><title>It works',
+            "</title><b>It works!</b><br /><br />Fuck yea! "]
