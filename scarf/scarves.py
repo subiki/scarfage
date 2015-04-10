144fbfa79b574f6a161e693e95ebe23da21bbdab
Chris Mazuc <chris@mazuc.net>
2015-04-10 16:55:48 -0700

    .

--- scarf/scarves.py
+++ scarf/scarves.py
@@ -3,9 +3,10 @@ from flask import redirect, url_for, request, render_template, session, escape, 
 from werkzeug import secure_filename
 from scarflib import check_login
 
-def get_upload(f):
+def get_upload(f, name):
     if not f.filename == '':
         flash('Uploaded ' + f.filename)
+        f.save('/srv/data/web/vhosts/default/static/uploads/' + secure_filename(name))
 
 @app.route('/scarf/<scarf_id>')
 def show_post(scarf_id):
@@ -21,15 +22,14 @@ def newscarf():
             flash('You must be logged in to create a scarf.')
             return redirect(url_for('/newuser'))
 
-
         if request.form['name'] == '':
             flash('This scarf has no name?')
             return redirect('/scarf/newscarf')
 
         flash('Adding scarf...')
 
-        get_upload(request.files['front'])
-        get_upload(request.files['back'])
+        get_upload(request.files['front'], "front" + escape(request.form['name']))
+        get_upload(request.files['back'], "back" + escape(request.form['name']))
 
         return redirect('/scarf/' + escape(request.form['name']))
 
