a6d977360859e89d70fe684b392c6c2f40604554
Chris Mazuc <chris@mazuc.net>
2015-04-10 20:01:52 -0700

    .

--- scarf/user.py
+++ scarf/user.py
@@ -1,9 +1,22 @@
 from scarf import app
 from flask import redirect, url_for, render_template, session, escape, request, flash
 from scarflib import check_login
+from urlparse import urlparse, urljoin
 
 #TODO change me
 app.secret_key = '\x8bN\xe5\xe8Q~p\xbdb\xe5\xa5\x894i\xb0\xd9\x07\x10\xe6\xa0\xe5\xbd\x1e\xf8'
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+           ref_url.netloc == test_url.netloc
+
+def redirect_back(endpoint, **values):
+    target = request.referrer
+    if not target or not is_safe_url(target):
+        target = url_for(endpoint, **values)
+    return redirect(target)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -19,7 +32,7 @@ def login():
         else:
             session['username'] = escape(request.form['username'])
             flash('You were successfully logged in')
-            return redirect(url_for('index'))
+            return redirect_back('index')
     return redirect(url_for('error'))
 
 @app.route('/newuser', methods=['GET', 'POST'])
@@ -41,7 +54,7 @@ def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
     flash('You were successfully logged out')
-    return redirect(url_for('index'))
+    return redirect_back('index')
 
 @app.route('/user/<username>')
 def show_user_profile(username):
