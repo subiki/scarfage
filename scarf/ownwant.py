20bef7fb58481990a0e30c1d2253d77bc86797e7
Chris Mazuc <chris@mazuc.net>
2015-04-21 19:27:46 -0700

    ownwant fixes, password reset and email update implemented

--- scarf/ownwant.py
+++ scarf/ownwant.py
@@ -1,60 +1,59 @@
 from scarf import app
 from flask import redirect, url_for, request, render_template, session, escape, flash
 from scarflib import check_login, check_scarf
-from sql import upsert, doupsert, read, doselect
+from sql import upsert, doupsert, read, doselect, delete
 from profile import get_userinfo
 from scarflib import redirect_back
 
-@app.route('/scarf/<scarf_id>/remove')
-def remove(scarf_id):
-    # TODO actually remove these at some point
-    ownwant(scarf_id, 4, 0)
-    ownwant(scarf_id, 5, 0)
-    ownwant(scarf_id, 6, 0)
-    ownwant(scarf_id, 7, 0)
-    return redirect_back('/scarf/scarf/' + escape(scarf_id))
+@app.route('/scarf/<scarf_id>/donthave')
+def donthave(scarf_id):
+    update = dict(willtrade=0, own=0)
+    ownwant(scarf_id, update)
+    return redirect_back('/scarf/' + escape(scarf_id))
 
 @app.route('/scarf/<scarf_id>/have')
 def have(scarf_id):
-    ownwant(scarf_id, 4, 1)
-    ownwant(scarf_id, 6, 0)
-    return redirect_back('/scarf/scarf/' + escape(scarf_id))
+    update = dict(own=1)
+    ownwant(scarf_id, update)
+    return redirect_back('/scarf/' + escape(scarf_id))
 
 @app.route('/scarf/<scarf_id>/hide')
 def hide(scarf_id):
-    ownwant(scarf_id, 5, 0)
-    ownwant(scarf_id, 7, 1)
-    return redirect_back('/scarf/scarf/' + escape(scarf_id))
+    update = dict(hide=1)
+    ownwant(scarf_id, update)
+    return redirect_back('/scarf/' + escape(scarf_id))
 
 @app.route('/scarf/<scarf_id>/show')
 def show(scarf_id):
-    ownwant(scarf_id, 7, 0)
-    return redirect_back('/scarf/scarf/' + escape(scarf_id))
+    update = dict(hide=0)
+    ownwant(scarf_id, update)
+    return redirect_back('/scarf/' + escape(scarf_id))
 
 @app.route('/scarf/<scarf_id>/wonttrade')
 def wonttrade(scarf_id):
-    ownwant(scarf_id, 5, 0)
-    return redirect_back('/scarf/scarf/' + escape(scarf_id))
+    update = dict(willtrade=0)
+    ownwant(scarf_id, update)
+    return redirect_back('/scarf/' + escape(scarf_id))
 
 @app.route('/scarf/<scarf_id>/willtrade')
 def willtrade(scarf_id):
-    ownwant(scarf_id, 4, 1)
-    ownwant(scarf_id, 5, 1)
-    ownwant(scarf_id, 7, 0)
-    return redirect_back('/scarf/scarf/' + escape(scarf_id))
+    update = dict(own=1, hidden=0, willtrade=1)
+    ownwant(scarf_id, update)
+    return redirect_back('/scarf/' + escape(scarf_id))
 
 @app.route('/scarf/<scarf_id>/want')
 def want(scarf_id):
-    ownwant(scarf_id, 4, 0)
-    ownwant(scarf_id, 6, 1)
+    update = dict(want=1)
+    ownwant(scarf_id, update)
     return redirect_back('/scarf/' + escape(scarf_id))
 
 @app.route('/scarf/<scarf_id>/dontwant')
 def dontwant(scarf_id):
-    ownwant(scarf_id, 6, 0)
+    update = dict(want=0)
+    ownwant(scarf_id, update)
     return redirect_back('/scarf/' + escape(scarf_id))
 
-def ownwant(scarf_id, index, value):
+def ownwant(scarf_id, values):
     scarf = check_scarf(escape(scarf_id))
     if scarf == False:
         return
@@ -76,33 +75,15 @@ def ownwant(scarf_id, index, value):
         except IndexError: 
             iuid=0
 
-        if index == 4:
-            sql = upsert("ownwant", \
-                         uid=iuid,
-                         userid=uid, \
-                         scarfid=scarf[1], \
-                         own=value)
+        update = dict(uid=iuid, userid=uid, scarfid=scarf[1])
+        update.update(values)
 
-        if index == 5:
-            sql = upsert("ownwant", \
-                         uid=iuid,
-                         userid=uid, \
-                         scarfid=scarf[1], \
-                         willtrade=value)
-
-        if index == 6:
-            sql = upsert("ownwant", \
-                         uid=iuid,
-                         userid=uid, \
-                         scarfid=scarf[1], \
-                         want=value)
-        if index == 7:
-            sql = upsert("ownwant", \
-                         uid=iuid,
-                         userid=uid, \
-                         scarfid=scarf[1], \
-                         hidden=value)
+        sql = upsert("ownwant", \
+                     **update)
 
         data = doupsert(sql)
+
+    sql = delete('ownwant', **{ 'own': '0', 'willtrade': '0', 'want': '0', 'hidden': '0' })
+    result = doselect(sql)
 
     return 
--- scarf/profile.py
+++ scarf/profile.py
@@ -1,20 +1,119 @@
 import os
 import uuid
 import datetime
+import re
 from scarf import app
 from flask import redirect, url_for, render_template, session, escape, request, flash
 from scarflib import check_login, redirect_back, pagedata, get_userinfo
 from sql import doupsert, upsert, doselect, read
 from main import page_not_found
+from user import check_pw, gen_pwhash
 
-@app.route('/pwreset')
-def pwreset():
+@app.route('/userupdate')
+def userupdate():
+    pd = pagedata()
     if 'username' in session:
-        pd = pagedata()
-        pd.title = title="Reset Password"
-        return render_template('pwreset.html', pd=pd)
-    else:
-        return redirect(url_for('index'))
+        pd.title = title="Update User Account"
+        return render_template('userupdate.html', pd=pd)
+
+    return redirect(url_for('index'))
+
+@app.route('/emailupdate', methods=['GET', 'POST'])
+def emailupdate():
+    pd = pagedata()
+    if 'username' in session:
+        if request.method == 'POST':
+            userinfo = get_userinfo(escape(session['username']))
+
+            try:
+                pd.userinfo=userinfo[0]
+                uid = userinfo[0][0]
+            except IndexError:
+                pd.title = "SQL Error"
+                pd.errortext = "SQL Error"
+                return render_template('error.html', pd=pd)
+
+            ret = False
+            if not check_pw(escape(session['username']), escape(request.form['password'])):
+                flash("Please check your password and try again")
+                ret = True
+
+            email = escape(request.form['email'])
+
+            if not re.match("[^@]+@[^@]+\.[^@]+", escape(request.form['email'])):
+                flash("Invalid email address")
+                ret = True
+
+            if ret:
+                return redirect_back(url_for('userupdate'))
+
+            sql = upsert("users", \
+                         uid=uid, \
+                         email=email)
+            app.logger.debug(sql)
+
+            data = doupsert(sql)
+
+            flash("Your email address has been changed.")
+            return redirect(url_for('userupdate'))
+
+    return redirect(url_for('index'))
+
+
+ 
+@app.route('/pwreset', methods=['GET', 'POST'])
+def pwreset():
+    pd = pagedata()
+    if 'username' in session:
+        if request.method == 'POST':
+            userinfo = get_userinfo(escape(session['username']))
+
+            try:
+                pd.userinfo=userinfo[0]
+                uid = userinfo[0][0]
+            except IndexError:
+                pd.title = "SQL Error"
+                pd.errortext = "SQL Error"
+                return render_template('error.html', pd=pd)
+
+            ret = False
+            if not check_pw(escape(session['username']), escape(request.form['password'])):
+                flash("Please check your current password and try again")
+                ret = True
+
+            pass1 = escape(request.form['newpassword'])
+            pass2 = escape(request.form['newpassword2'])
+
+            if pass1 != pass2:
+                flash("The passwords entered don't match.")
+                ret = True
+
+            if len(pass1) < 6:
+                flash("Your new password is too short, it must be at least 6 characters")
+                ret = True
+
+            if ret:
+                return redirect_back(url_for('userupdate'))
+
+            salt=str(uuid.uuid4().get_hex().upper()[0:6])
+            app.logger.debug(salt)
+
+            app.logger.debug(request.form)
+            pwhash=gen_pwhash(escape(request.form['newpassword']), salt)
+            app.logger.debug(pwhash)
+
+            sql = upsert("users", \
+                         uid=uid, \
+                         pwhash=pwhash, \
+                         pwsalt=salt)
+            app.logger.debug(sql)
+
+            data = doupsert(sql)
+
+            flash("Your password has been reset.")
+            return redirect(url_for('userupdate'))
+
+    return redirect(url_for('index'))
 
 @app.route('/user/<username>')
 def show_user_profile(username):
