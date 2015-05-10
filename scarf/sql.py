2559a27f9f08b19c206ac3c922553825512cbf86
subiki <karmicj@gmail.com>
2015-05-09 20:49:03 -0700

    path sanitized

--- scarf/sql.py
+++ scarf/sql.py
@@ -5,8 +5,6 @@ import datetime
 
 if socket.gethostname() == "grenadine":
     dbHost = 'sql.home.mazuc.net'
-elif socket.gethostname()=="prefect":
-    dbHost = '127.0.0.1'
 else: 
     dbHost = 'localhost'
 
