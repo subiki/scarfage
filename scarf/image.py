c1da3cc5bcf6099d903e48ef096fffa1443af63f
Chris Mazuc <chris@mazuc.net>
2015-06-12 22:01:53 -0700

    bugfix

--- scarf/image.py
+++ scarf/image.py
@@ -92,7 +92,7 @@ def serve_pil_image(pil_img):
 
 #TODO this is broken, I hate it
 img_resize_cache = dict()
-@memoize_with_expiry(img_resize_cache, long_cache_persist)
+#@memoize_with_expiry(img_resize_cache, long_cache_persist)
 def resize(img, maxwidth, maxheight):
     hsize = img.size[0]
     vsize = img.size[1]
@@ -123,21 +123,21 @@ def resize(img, maxwidth, maxheight):
 img_cache = dict()
 @app.route('/image/<image>/thumbnail')
 def serve_thumb(image):
-    #try:
-    img=Image.open(upload_dir + escape(image))
-    img = resize(img, 800.0, 200.0)
-    return serve_pil_image(img)
-    #except:
-    #    return page_not_found(404)
+    try:
+        img=Image.open(upload_dir + escape(image))
+        img = resize(img, 800.0, 200.0)
+        return serve_pil_image(img)
+    except IOError:
+        return page_not_found(404)
 
 @app.route('/image/<image>/preview')
 def serve_preview(image):
-    #try:
-    img=Image.open(upload_dir + escape(image))
-    img = resize(img, 800.0, 800.0)
-    return serve_pil_image(img)
-    #except:
-    #    return page_not_found(404)
+    try:
+        img=Image.open(upload_dir + escape(image))
+        img = resize(img, 800.0, 800.0)
+        return serve_pil_image(img)
+    except IOError:
+        return page_not_found(404)
 
 @app.route('/image/<img_id>')
 def show_image(img_id):
