f5f24de52199075fbe01b8b7c18481c695840e53
Chris Herz <(null)>
2015-09-09 23:42:32 -0700

    initial css updates...

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+// var process     = require('child_process');
+
+/* START FLASK SERVER
+gulp.task('flask', function(){
+  var spawn = process.spawn;
+  console.info('Starting flask server');
+  var PIPE = {stdio: 'inherit'};
+  spawn('python', ['../run.py','runserver'], PIPE);
+});
+*/
+
+/* COMPILE SCSS ON CHANGE */
 gulp.task('styles', function() {
     gulp.src('./scarf/precompile/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./scarf/static/css/'))
 });
 
-//Watch task
+/* WATCH FOR CHANGES ON SCSS */
 gulp.task('default',function() {
-    gulp.watch('./scarf/precompile/scss/*.scss',['styles']);
+  gulp.watch('./scarf/precompile/scss/*.scss',['styles']);
 });
