faef2b7d3a729df80db9ba84de3bad8df87f32ad
Chris Herz <(null)>
2016-02-06 12:30:16 -0800

    moving site stat to homepage.

--- scarf/main.py
+++ scarf/main.py
@@ -2,7 +2,7 @@
 from scarf import app
 from flask import render_template, session, request, flash
 from sql import doquery, read
-from scarflib import pagedata, latest_items
+from scarflib import pagedata, latest_items, get_whores_table, get_contribs_table, get_needy_table, get_willtrade_table
 from nocache import nocache
 
 @app.errorhandler(404)
@@ -41,6 +41,10 @@ def index():
     pd = pagedata()
     pd.title = "Scarfage"
 
+    pd.topcollectors = get_whores_table()
+    pd.topcontributors = get_contribs_table()
+    pd.topneedy = get_needy_table()
+    pd.topwilltrade = get_willtrade_table()
     pd.items = latest_items(50)
 
     return render_template('index.html', pd=pd)
