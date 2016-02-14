be59b34cc55238e7321bb0306f027b8ab35899a7
Chris Herz <herzbrew@gmail.com>
2016-02-14 00:50:28 -0800

    updated gulpfile.js to concat and minify CSS into one file: style.min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
+var concat = require('gulp-concat');
 
 gulp.task('default', function(){
 
   gulp.task('sass', function(){
     return gulp.src('scarf/static/assets/sass/*.scss')
       .pipe(sass()) // Converts Sass to CSS with gulp-sass
+      .pipe(concat('style.min.css'))
+      .pipe(cssnano())
       .pipe(gulp.dest('scarf/static/'))
   });
 
-  gulp.task('cssnano', function() {
-    return gulp.src('scarf/static/*.css')
-      .pipe(cssnano())
-      .pipe(gulp.dest('scarf/static/min/'));
-  });
-  gulp.watch('scarf/static/assets/sass/*.scss', ['sass']);
-  gulp.watch('scarf/static/*.css', ['cssnano']);
+  gulp.watch('scarf/static/assets/sass/*css', ['sass']);
 });
