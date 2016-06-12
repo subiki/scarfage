8a2664f8a8459159bfbfab0cc8f7741699436852
Chris Herz <(null)>
2016-06-11 17:10:29 -0700

    login form tweaks

--- scarf/static/nav.js
+++ scarf/static/nav.js
@@ -1,3 +1,9 @@
+// always show login on mobile
+if ($(window).width() < 767) {
+  $('.login-container').show();
+}
+
+// close login
 function closeLogin() {
   $('.login-container').toggle();
   return;
